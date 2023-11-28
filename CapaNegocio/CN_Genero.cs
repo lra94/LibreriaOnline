d9131be239514db69f990e71354fc1cb81ca7435
lrosario <lrosario@DESKTOP-BK4ED8O>
2023-11-27 20:24:50 -0400

    Se agrega funcionalidad para mantenimiento de Generos

---
+++ CapaNegocio/CN_Genero.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Genero
+    {
+        private CD_Genero objCapaDato = new CD_Genero();
+
+
+        public List<Genero> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+
+        //Metodo Registrar Generos
+        public int Registrar(Genero obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "La descripcion del Genero no puede estar vacio!";
+            }
+            
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+               return objCapaDato.Registrar(obj, out Mensaje);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //Editar Genero
+        public bool Editar(Genero obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "La descripcion del Genero no puede estar vacio!";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Eliminar Genero
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return objCapaDato.Eliminar(id, out Mensaje);
+        }
+    }
+}
--- CapaPresentacionAdmin/Controllers/MantenedorController.cs
+++ CapaPresentacionAdmin/Controllers/MantenedorController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CapaEntidad;
+using CapaNegocio;
+
 namespace CapaPresentacionAdmin.Controllers
 {
     public class MantenedorController : Controller
@@ -24,6 +27,43 @@ namespace CapaPresentacionAdmin.Controllers
         public ActionResult Producto()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarGeneros()
+        {
+            List<Genero> oLista = new List<Genero>();
+
+            oLista = new CN_Genero().Listar();
+
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarGenero(Genero objeto)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+
+            if (objeto.IdGenero == 0)
+            {
+                resultado = new CN_Genero().Registrar(objeto, out mensaje);
+            }
+            else
+            {
+                resultado = new CN_Genero().Editar(objeto, out mensaje);
+            }
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult EliminarGenero(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new CN_Genero().Eliminar(id, out mensaje);
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
         }
     }
 }
