db5fe93c419f539fb763df4234884ba1c2f61040
lrosario <lrosario@DESKTOP-BK4ED8O>
2023-12-05 15:27:24 -0400

    Se agregaron las funcionalidades para que los clientes puedan iniciar sesion, cambiar clave y recuperar claves

---
+++ CapaNegocio/CN_Cliente.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using CapaDatos;
+using CapaEntidad;
+
+
+namespace CapaNegocio
+{
+    public class CN_Cliente
+    {
+        private CD_Cliente objCapaDato = new CD_Cliente();
+
+        public int Registrar(Cliente obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Nombre) || string.IsNullOrWhiteSpace(obj.Nombre))
+            {
+                Mensaje = "El nombre no puede estar vacio!";
+            }
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+            {
+                Mensaje = "El Apellido no puede estar vacio!";
+            }
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+            {
+                Mensaje = "El Correo no puede estar vacio!";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                obj.Clave = CN_Recursos.ConvertirShat256(obj.Clave);
+                return objCapaDato.Registrar(obj, out Mensaje);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public List<Cliente> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+
+        //Cambiar Clave de Cliente
+        public bool CambiarClave(int idcliente, string nuevaclave, out string Mensaje)
+        {
+            return objCapaDato.CambiarClave(idcliente, nuevaclave, out Mensaje);
+        }
+
+        //Reestablecer Clave de Cliente
+        public bool ReestablecerClave(int idcliente, string correo, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+            string nuevaclave = CN_Recursos.GenerarClaveAleatoria();
+            bool resultado = objCapaDato.ReestablecerClave(idcliente, CN_Recursos.ConvertirShat256(nuevaclave), out Mensaje);
+
+            if (resultado)
+            {
+                string asunto = "Libreria Online - Reestablecer clave";
+                string mensaje_correo = $"<h3>Su cuenta fue reestablecida correctamente</h3></br><p>Su nueva clave temporal es: {nuevaclave}</p>";
+                bool respuesta = CN_Recursos.EnviarCorreo(correo, asunto, mensaje_correo);
+
+                if (respuesta)
+                {
+                    return true;
+                }
+                else
+                {
+                    Mensaje = "No se pudo enviar el correo.";
+                    return false;
+                }
+            }
+            else
+            {
+                Mensaje = "No se pudo reestablecer la clave.";
+                return false;
+            }
+
+        }
+    }
+}
---
+++ CapaPresentacionTienda/Controllers/AccesoController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using CapaEntidad;
+using CapaNegocio;
+
+namespace CapaPresentacionTienda.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        public ActionResult Reestablecer()
+        {
+            return View();
+        }
+
+        public ActionResult CambioClave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Cliente objeto)
+        {
+            int resultado;
+            string mensaje = string.Empty;
+
+            ViewData["Nombre"] = string.IsNullOrEmpty(objeto.Nombre) ? "" : objeto.Nombre;
+            ViewData["Apellidos"] = string.IsNullOrEmpty(objeto.Apellidos) ? "" : objeto.Apellidos;
+            ViewData["Correo"] = string.IsNullOrEmpty(objeto.Correo) ? "" : objeto.Correo;
+
+            if (objeto.Clave != objeto.ConfirmarClave)
+            {
+                ViewBag.Error = "Las claves no coinciden!";
+                return View();
+            }
+
+            resultado = new CN_Cliente().Registrar(objeto, out mensaje);
+
+            if (resultado > 0)
+            {
+                ViewBag.Error = null;
+
+                return RedirectToAction("Index", "Acceso");
+            }
+            else
+            {
+                ViewBag.Error = mensaje;
+                return View();
+            }
+
+         
+        }
+
+        [HttpPost]
+        public ActionResult Index(string correo, string clave)
+        {
+            Cliente oCliente = null;
+
+            oCliente = new CN_Cliente().Listar().Where(item => item.Correo == correo && item.Clave == CN_Recursos.ConvertirShat256(clave)).FirstOrDefault();
+
+            if (oCliente == null)
+            {
+                ViewBag.Error = "Correo o Clave no son correctos!";
+                return View();
+            }
+            else
+            {
+                if (oCliente.Reestablecer)
+                {
+                    TempData["IdCliente"] = oCliente.IdCliente;
+                    return RedirectToAction("CambioClave","Acceso");
+                }
+                else
+                {
+                    FormsAuthentication.SetAuthCookie(oCliente.Correo,false);
+                    Session["Cliente"] = oCliente;
+
+                    ViewBag.Error = null;
+                    return RedirectToAction("Index","Tienda");
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Reestablecer(string correo)
+        {
+            Cliente oCliente = new Cliente();
+
+            oCliente = new CN_Cliente().Listar().Where(item => item.Correo == correo).FirstOrDefault();
+
+            if (oCliente == null)
+            {
+                ViewBag.Error = "No existe un Cliente relacionado a ese correo!";
+                return View();
+            }
+
+            string mensaje = string.Empty;
+            bool respuesta = new CN_Cliente().ReestablecerClave(oCliente.IdCliente, correo, out mensaje);
+
+            if (respuesta)
+            {
+                ViewBag.Error = null;
+                return RedirectToAction("Index", "Acceso");
+            }
+            else
+            {
+                ViewBag.Error = mensaje;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CambioClave(string idcliente, string claveactual, string nuevaclave, string confirmarclave)
+        {
+            Cliente oUsuario = new Cliente();
+
+            oUsuario = new CN_Cliente().Listar().Where(u => u.IdCliente == int.Parse(idcliente)).FirstOrDefault();
+
+            if (oUsuario.Clave != CN_Recursos.ConvertirShat256(claveactual))
+            {
+                TempData["IdCliente"] = idcliente;
+
+                ViewBag.Error = "La clave actual no es correcta!";
+                ViewData["vclave"] = "";
+                return View();
+            }
+            else if (nuevaclave != confirmarclave)
+            {
+                TempData["IdCliente"] = idcliente;
+                ViewData["vclave"] = claveactual;
+                ViewBag.Error = "Las claves no coinciden!";
+                return View();
+            }
+
+            ViewData["vclave"] = "";
+            nuevaclave = CN_Recursos.ConvertirShat256(nuevaclave);
+
+            string mensaje = string.Empty;
+
+            bool respuesta = new CN_Cliente().CambiarClave(int.Parse(idcliente), nuevaclave, out mensaje);
+
+            if (respuesta)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["IdCliente"] = idcliente;
+                ViewBag.Error = mensaje;
+                return View();
+            }
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Acceso");
+        }
+    }
+}
